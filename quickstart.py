f946799b94f029a20fbe7662b1a33a6c7be147ef
Andrei Chenchik <andrei@chenchik.me>
2021-04-19 19:17:53 +0200

    Flask Tutorial

---
+++ quickstart.py
@@ -0,0 +1,48 @@
+from flask import Flask, url_for, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f"Post {post_id}"
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f"Subpath {escape(subpath)}"
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+    print(url_for('static', filename='style.css'))
+
